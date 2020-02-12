a2773b34640a05c10f1b716d348cd94c4728f138
zyx124 <zhaoyuxin124@gmail.com>
2020-02-11 22:44:34 -0500

    add province parser and area info parser

--- spider.py
+++ spider.py
@@ -35,7 +35,6 @@ class Spider:
             sum_info = re.search(r'\{("id".*?)\]\}', str(soup.find('script', attrs={'id': 'getStatisticsService'})))
             province_info = re.search(r'\[(.*?)\]', str(soup.find('script', attrs={'id': 'getListByCountryTypeService1'})))
             area_info = re.search(r'\[(.*)\]', str(soup.find('script', attrs={'id': 'getAreaStat'})))
-            abroad_info = re.search(r'\[(.*)\]', str(soup.find('script', attrs={'id': 'getListByCountryTypeService2'})))
 
             if not any([sum_info, province_info, area_info, abroad_info]):
                 continue
@@ -51,8 +50,31 @@ class Spider:
         sum_info.pop('modifyTime')
         sum_info.pop('imgUrl')
         sum_info.pop('deleted')
+        sum_info.pop('')
         sum_info['countRemark'] = sum_info['countRemark']
-        print(sum_info)
+        if not self.db.find_one(collection='Sum_info', data=sum_info):
+            sum_info['updateTime'] = self.timestamp
+            self.db.insert(collection="Sum_info", data=sum_info)
+
+    def province_info_parser(self, province_info):
+        province = json.loads(province_info.group(0))
+        for p in province:
+            p.pop('id')
+            p.pop('tags')
+            p.pop('sort')
+            if self.db.find_one(collection='Province', data=p):
+                continue
+            p['crawlTime'] = self.timestamp
+            p['country'] = 'China'
+
+    def area_info_parser(self, area_info):
+        area = json.loads(area_info.group(0))
+        for a in area:
+            if self.db.find_one(collection='Area', data=a):
+                continue
+            area['country'] = 'China'
+            area['updateTime'] = self.timestamp
+
 
 if __name__ == "__main__":
     s = Spider()
